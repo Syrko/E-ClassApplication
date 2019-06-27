3626c0ca015e242cd35a037a537b80dbe8406619
Syrko <kostassir@yahoo.gr>
2019-06-27 22:25:33 +0300

    Assuring compliance

--- CSharp_Code/E_Class/E_Class/FormLogin.cs
+++ CSharp_Code/E_Class/E_Class/FormLogin.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace E_Class
 {
     public partial class FormLogin : Form
     {
-        private static int gammaA = 255;
-        public FormLogin()
+		private static int gammaA = 255;
+
+		private AbstractFormFactory abstractFormFactory;
+
+		public FormLogin()
         {
             InitializeComponent();
         }
