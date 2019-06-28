a15f8630152a3c4cacccfff5c47e64fedd45a870
Syrko <kostassir@yahoo.gr>
2019-06-28 20:12:54 +0300

    New shell for Professor

--- CSharp_Code/E_Class/E_Class/Professor.cs
+++ CSharp_Code/E_Class/E_Class/Professor.cs
@@ -52,8 +52,28 @@ namespace E_Class
 		private List<Course> courseList;
 
 		// Constructors
+		public Professor(RegNum registrationNumber, string password, string name, string surname, Email email, List<Course> courseList)
+		{
+			this.registrationNumber = registrationNumber;
+			this.password = password;
+			this.name = name;
+			this.surname = surname;
+			this.email = email;
+
+			this.courseList = courseList;
+		}
 
 		// Methods
-
+		public void createProject(Project newProject);
+		public void editProject(Project project);
+		public void deleteProject(Project project);
+		public void createTeam(Team newTeam);
+		public void editTeam(Team team);
+		public void deleteTeam(Team team);
+		public void gradeProject(Project project);
+		
+		// Setters - Getters
+		public List<Course> getCourseList() { return courseList; }
+		public void setCourseList(List<Course> courseList) { this.courseList = courseList; }
 	}
 }
